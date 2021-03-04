59aad238db2f928658bafdce7102ca428a99c9b9
Collin Pfeifer <collinpfeifer@Icloud.com>
2021-03-03 22:05:54 -0500

    finished up most of server side and apollo server
    finished up most of server side and apollo graphql sever

---
+++ habittracker-server/index.ts
@@ -0,0 +1,12 @@
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './graphql/typeDefs';
+import { resolvers } from './graphql/resolvers/tasks';
+
+export const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
